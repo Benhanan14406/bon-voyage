32106743250c9cb289ce6a278562b4f03654da46
angelobenhanan <angelo.benhanan@ui.ac.id>
2025-07-16 17:42:40 +0700

    Membuat model Travel

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Travel(models.Model):
+    currentLoc = models.CharField(max_length=1000, default="", editable=True)
+    destination = models.CharField(max_length=1000, default="", editable=True)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    travelTime = models.IntegerField(default=0, db_default=0, editable=True)
+    travelMode = models.CharField(max_length=10, default="", editable=True)
+    travelError = models.IntegerField(default=0, db_default=0, editable=True)
